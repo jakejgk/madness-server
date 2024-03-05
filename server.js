b04bc94430c5ca723e34c909ffec8b825beba363
Jake <jglacer612@gmail.com>
2024-03-04 20:21:03 -0500

    hmm

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const db = require("./db"); // Import the database connection
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 // Middleware to parse JSON bodies
 app.use(express.json());
 
@@ -13,6 +15,23 @@ app.get("/madness", async (req, res) => {
     res.json(rows);
   } catch (error) {
     console.error("Error fetching users", error);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.post("/submit", async (req, res) => {
+  const { email, steak } = req.body;
+
+  try {
+    const queryText =
+      "INSERT INTO madness(email, steak) VALUES($1, $2) RETURNING *";
+    const values = [email, JSON.stringify(steak)];
+
+    const { rows } = await db.query(queryText, values);
+    // const { rows } = await db.query(queryText, values);
+    res.json(rows[0]);
+  } catch (error) {
+    console.error("Error saving to database", error);
     res.status(500).send("Server Error");
   }
 });
